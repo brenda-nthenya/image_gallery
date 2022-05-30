b9acb5fcfa6f14d7be76e5416c9937975b83c3bb
Brenda <bee@bee.com>
2022-05-30 13:16:18 +0300

    Corrects the get_location function test and test save category function

--- image_gallery/tests.py
+++ image_gallery/tests.py
@@ -11,7 +11,7 @@ class TestLocation(TestCase):
     def test_instance(self):
         self.assertTrue(isinstance(self.location, Location))
 
-    def test_save_location(self):
+    def test_get_location(self):
         self.location.save_location()
         locations=Location.get_locations()
         self.assertTrue(len(locations) > 0)
@@ -20,7 +20,12 @@ class TestCategory(TestCase):
 
     def setUp(self):
         self.category = Category(name='home')
-        self.category.save_category
+        self.category.save_category()
 
     def test_instance(self):
         self.assertTrue(isinstance(self.category, Category))
+
+    def test_save_category(self):
+        self.category.save_category()
+        categories=Category.objects.all()
+        self.assertTrue(len(categories)> 0)
