f8f6f72d2d9290cf26b6aee3bb2a20d64e8384b9
Brenda <bee@bee.com>
2022-05-30 12:35:28 +0300

    Create model Image

--- image_gallery/models.py
+++ image_gallery/models.py
@@ -9,4 +9,11 @@ class Location(models.Model):
 class Category(models.Model):
     name = models.CharField(max_length=40)
 
-
+class Image(models.Model):
+    image = models.ImageField(upload_to='images/')
+    name = models.CharField(max_length=60)
+    description = models.TextField()
+    photographer = models.CharField(max_length=40, default='admin')
+    date = models.DateTimeField(auto_now_add=True)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    location = models.ForeignKey(Location, on_delete=models.CASCADE)
