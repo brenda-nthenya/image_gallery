752d0dabbef3fe9b65ec6b00399f8ebcab0e6b94
Brenda <bee@bee.com>
2022-05-31 23:29:37 +0300

    Adds the image view function

--- image_gallery/views.py
+++ image_gallery/views.py
@@ -1,12 +1,14 @@
 from django.shortcuts import render
-from django.http import HttpResponse 
+from django.http import HttpResponse, Http404
 from .models import *
+from django.core.exceptions import ObjectDoesNotExist
 
 # Create your views here.
 def index(request):
     image = Image.objects.all()
     
     categories = Category.objects.all()
+
 
     context= {
         "image": image,
@@ -17,18 +19,23 @@ def index(request):
 
 def search_results(request):
 
-    if 'imagesearch' in request.GET and request.GET["imagesearch"]:
-        category = request.GET.get("imagesearch")
-        images = Image.search_by_category(category)
-        message = f"{category}"
+    if 'category' in request.GET and request.GET["category"]:
+        search_term = request.GET.get("category")
+        searched_image = Image.search_by_category(search_term)
+        message = f"{search_term}"
         context = {
             "message": message, 
-            "images": images
+            "images": searched_image
 
         }
-
-
         return render(request, 'gallery/search_res.html', context)
     else:
         message = "You haven't searched for any image category"
         return render(request, 'gallery/search_res.html', {"message": message})
+
+def image(request,image_id):
+    try:
+        image = Image.objects.get(id = image_id)
+    except ObjectDoesNotExist:
+        raise Http404()
+    return render(request,"all-photo/image.html", {"image":image})
